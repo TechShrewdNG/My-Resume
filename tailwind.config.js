11256ca68ed0c8306315061c0dcbb26030b85122
Nnamdi_Awurumibe <nnamdi912@outlook.com>
2021-09-01 16:12:53 +0100

    e

--- tailwind.config.js
+++ tailwind.config.js
@@ -4,7 +4,7 @@ module.exports = {
   theme: {
     colors: {
       primary: colors.blue,
-      secondary: colors.orange,
+      secondary: colors.red,
       neutral: colors.gray,
       white: '#FFF',
     },
