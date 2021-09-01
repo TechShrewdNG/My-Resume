32a64ad011af6a6b3bc38326d4e9842478acc96a
Nnamdi_Awurumibe <nnamdi912@outlook.com>
2021-09-01 16:19:49 +0100

    p

--- tailwind.config.js
+++ tailwind.config.js
@@ -3,12 +3,11 @@ const { colors } = require('tailwindcss/defaultTheme');
 module.exports = {
   theme: {
     colors: {
-      blue: {
+      primary: {
         light: '#0000ff',
         DEFAULT: '#0000ff',
         dark: '#0000ff',
       },
-      primary: colors.blue,
       secondary: colors.red,
       neutral: colors.gray,
       white: '#FFF',
